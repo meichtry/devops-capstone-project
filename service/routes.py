db5bbe5b607977b8266e841dbf0af427d96fdfab
meichtry <84460330+meichtry@users.noreply.github.com>
2024-01-03 15:08:32 +0100

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -57,6 +57,7 @@ def create_accounts():
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
 
+
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
