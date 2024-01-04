ba14e3f17a0f3fd6d452fe9a8804994cccdc7663
Cedric <cedric.meichtry@gmail.com>
2024-01-04 11:21:30 -0500

    added link task

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -186,7 +186,6 @@ class TestAccountService(TestCase):
         resp = self.client.delete(BASE_URL)
         self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
-
     # Security Test cases
     def test_security_headers(self):
         """It should return security headers"""
@@ -201,7 +200,6 @@ class TestAccountService(TestCase):
         for key, value in headers.items():
             self.assertEqual(response.headers.get(key), value)
 
-    
     def test_cors_security(self):
         """It should return a CORS header"""
         response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
